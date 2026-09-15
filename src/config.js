68c37356c2169fb4864e923543f1a18f4e98210e
TrongLe <trongleele2@gmail.com>
2026-09-15 16:35:14 +0700

    update

--- src/config.js
+++ src/config.js
@@ -17,7 +17,7 @@ export const LETTER_CONTENT = {
     "Anh yêu em nhiều lắm Mơ à, e có biết không, yêu em rất rất nhiều hơn tất thẩy mọi thứ trên đời này.Hạnh phúc khi có em ở bên cạnh",
     "Tháng này a thấy e đã vui vẻ hơn, cười nhiều hơn và hạnh phúc hơn bao giờ hết. Đúng là môi trường ảnh hưởng đến tâm trạng nhiều quá ha :ddd",
     "Quen em anh biết bày tỏ mình nhiều hơn, nói ra nhiều cảm xúc trong lòng hơn những điều mà như a trước đây chưa nói ra giờ đã bày tỏ cho em tất cả. Cảm ơn e vì đã cho a hiểu ra điều đó, yêu nhau sẽ thẳng thắng nói chuyện với nhau.",
-    "Đây có thể là lần cuối anh viết thư như vậy, có thể sẽ viết vào 1 vài dịp đặc biệt khác nhưng sẽ ko như z nưa, như trước anh viết là bày tỏ những suy nghĩ mà mình chưa nói ra cho e biết, nhưng từ giờ sẽ không cần phải v nữa rùi. A cũng mong e sẽ viết 1 bức thư gì đó gửi cho anh, cho anh được lưu giữ những lời nói của e vào sâu thâm",
+    "Đây có thể là lần cuối anh viết thư như vậy, có thể sẽ viết vào 1 vài dịp đặc biệt khác, như trước anh viết là bày tỏ những suy nghĩ mà mình chưa nói ra cho e biết, nhưng từ giờ sẽ không cần phải v nữa rùi. A cũng mong e sẽ viết 1 bức thư gì đó gửi cho anh, cho anh được lưu giữ những lời nói của e vào sâu thẳm trong trái tim loạn nhịp vì e này",
     "Cuối cùng thì anh chỉ muốn nói với e là, anh yêu em rất rất nhiều, mãi yêu em đến khi nào,... Đến khi nào mặt trời ngừng chiếu!",
   ],
   closing: "Yêu em forever 💝",
